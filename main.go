14e5daeb34919568032c7c044d521acf431e1d0a
Robert Mulley <rmulley@fastmail.fm>
2014-10-05 23:24:42 -0400

    Successfully extracting tag name and CSS rule.  Need to extract ID and class next

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import (
 	"flag"
 	"log"
 	"os"
+	"regexp"
 ) //import
+
+type CssRule struct {
+	Rule  string
+	Value string
+} //struct
 
 // Global variables
 var (
@@ -18,24 +24,71 @@ func init() {
 	flag.StringVar(&I_FILENAME, "ifile", "", "Unable to set I_FILENAME flag variable.")
 	flag.Parse()
 
-	//HOST = strings.TrimSpace(strings.ToLower(HOST))
+	if I_FILENAME == "" {
+		log.Fatalln("ifile parameter must be specificed.")
+	} //if
 } //init
 
 func main() {
 	var (
-		err   error
-		lines []string
+		err           error
+		lines         []string
+		allCssRules   = make(map[string][]CssRule)
+		findInlineCSS = regexp.MustCompile("<(\\w+)\\s.*?style=\"([\\w\\-]+):([\\w]+);\"")
 	) //var
 
 	// Extract each line of the file
 	if lines, err = readFile(I_FILENAME); err != nil {
 		log.Fatalln(err)
 	} //if
-
+	ctr := 0
 	for _, line := range lines {
+		if ctr == 28 {
+			// Determine if
+			if allCssRules = extractInlineCSS(line, findInlineCSS); err != nil {
+				log.Println(err)
+			} //if
+		} //if
 
+		ctr++
+	} //for
+
+	for k, v := range allCssRules {
+		log.Println(k)
+		log.Println(v)
+		log.Println("")
 	} //for
 } //main
+
+func extractInlineCSS(line string, findInlineCSS *regexp.Regexp) (cssRules map[string][]CssRule) {
+	log.Println(line)
+	cssRules = make(map[string][]CssRule)
+	styles := findInlineCSS.FindAllStringSubmatch(line, -1)
+
+	for _, v := range styles {
+		var tag string
+
+		for pos, _ := range v {
+			// First position is entire capture
+			if pos != 0 {
+				// Second position is tag name
+				if pos == 1 {
+					tag = v[pos]
+				} else if pos%2 == 0 {
+					// Even positions are rules
+					// Odd positions are values
+					cssRules[tag] = append(cssRules[tag],
+						CssRule{
+							Rule:  v[pos],
+							Value: v[pos+1],
+						}) //append
+				} //elseif
+			} //if
+		} //for
+	} //for
+
+	return cssRules
+} //extractInlineCSS
 
 func readFile(filename string) (lines []string, err error) {
 	var (
